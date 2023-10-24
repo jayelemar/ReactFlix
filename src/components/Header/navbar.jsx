c598c5b66128d1fedc7b7c172339c2961ef3e5d1
Jay Termulo <jetermulo@gmail.com>
2023-10-25 03:10:15 +0800

    style: introPage.jsx navbar - responsive design

--- src/components/Header/navbar.jsx
+++ src/components/Header/navbar.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 const StyledNavbar = styled.nav`
   position: sticky;
@@ -31,24 +32,46 @@ const TextLogo = styled.div`
 `;
 
 const NavLinks = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
+  display: ${props => props.isMenuOpen ? 'flex-col' : 'none'};
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 1);
+  margin: auto;
+  padding: 25rem 3rem;
   list-style: none;
   justify-content: center;
   flex-grow: 1;
+
+  @media screen and (min-width: 640px) {
+      display: flex;
+      position: relative;
+      height: auto;
+      width: auto;
+      background-color: transparent;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      justify-content: center;
+      flex-grow: 1;
+    }
 `;
 
 const NavLinkItem = styled.li`
-  margin-left: 30px;
   position: relative;
   font-family: "Montserrat", sans-serif;
   background-color: transparent;
   border-radius: 25%;
-  padding: 5px 10px;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const NavLink = styled(Link)`
+  
   color: white;
   text-decoration: none;
   font-size: 20px;
@@ -56,13 +79,44 @@ const NavLink = styled(Link)`
 
 const SubscribeButton = styled(NavLink)`
   background-color: #e50914;
-  border-radius: 25px;
+  border-radius: 6px;
   height: 40px;
   width: 120px;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 10px;
+  margin: 0px;
+
+  @media screen and (min-width: 640px) {
+    display: flex;
+  }
+`;
+
+const MenuIcon = styled(AiOutlineMenu)`
+  font-size: 30px;
+  color: white;
+  margin: 0 1rem;
+  display: flex;
+  cursor: pointer;
+
+  @media screen and (min-width: 640px) {
+    display: none;
+  }
+`;
+
+const CloseIcon = styled(AiOutlineClose)`
+  position: absolute;
+  font-size: 30px;
+  color: white;
+  margin: 0 1rem;
+  display: flex;
+  cursor: pointer;
+  top: 2.5rem;
+  right: 0;
+
+  @media screen and (min-width: 640px) {
+    display: none;
+  }
 `;
 
 const Modal = styled.div`
@@ -108,43 +162,61 @@ const ModalContentCentered = styled.div`
   text-align: center;
 `;
 
+
+
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    console.log(isMenuOpen);
+  };
+
 
   return (
     <>
       <StyledNavbar>
         <TextLogo style={{ marginLeft: "3%" }}>ReactFlix</TextLogo>
-        <NavLinks>
-          <NavLinkItem>
-            <NavLink to="/home">Browse</NavLink>
-          </NavLinkItem>
+        <NavLinks isMenuOpen={isMenuOpen}>
+            <NavLinkItem>
+              <NavLink to="/home">Browse</NavLink>
+            </NavLinkItem>
 
-          <NavLinkItem>
-            <NavLink to="/reg">Login</NavLink>
-          </NavLinkItem>
+            <NavLinkItem>
+              <NavLink to="/reg">Login</NavLink>
+            </NavLinkItem>
+            <CloseIcon size={30} onClick={closeMenu}/>
         </NavLinks>
         <NavLinkItem style={{ marginRight: "3%" }}>
-          <SubscribeButton onClick={toggleModal}>Subscribe</SubscribeButton>
-          <Modal isOpen={isModalOpen}>
-            <ModalContent>
-              <CloseButton onClick={toggleModal}>&times;</CloseButton>
-              <ModalContentCentered>
-                <p>Want to Avail 5$ Lifetime No Ads?</p>
+            <SubscribeButton onClick={toggleModal} style={{zIndex: '1'}}>Subscribe</SubscribeButton>
+            
+            <Modal isOpen={isModalOpen}>
+              <ModalContent>
+                <CloseButton onClick={toggleModal}>&times;</CloseButton>
+                <ModalContentCentered>
+                  <p>Want to Avail 5$ Lifetime No Ads?</p>
+                  <Line />
+                  <p>Or</p>
+                  <Line />
+                  <p>Want to Avail 1$ 1 Month No Ads?</p>
+                </ModalContentCentered>
                 <Line />
-                <p>Or</p>
-                <Line />
-                <p>Want to Avail 1$ 1 Month No Ads?</p>
-              </ModalContentCentered>
-              <Line />
-              <SubscribeModalButton>Subscribe Now</SubscribeModalButton>
-            </ModalContent>
-          </Modal>
+                <SubscribeModalButton>Subscribe Now</SubscribeModalButton>
+                
+              </ModalContent>
+            </Modal>
         </NavLinkItem>
+        <MenuIcon size={30} onClick={toggleMenu}/>
       </StyledNavbar>
     </>
   );
